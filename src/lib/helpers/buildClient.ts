c75020dcee87694f7922d1d958071f8d13ffe203
victand98 <victandres98@gmail.com>
2022-02-27 18:18:32 -0500

    Updating client http config

--- src/lib/helpers/buildClient.ts
+++ src/lib/helpers/buildClient.ts
@@ -1,18 +1,17 @@
-import axios, { AxiosRequestHeaders } from "axios";
+import axios from "axios";
 import { NextPageContext } from "next";
 import { SERVER_URI, SERVER_URI_PRIVATE } from ".";
 
 const buildClient = ({ req }: NextPageContext) => {
-  console.log("\nREQ.HEADERS\n", req?.headers);
+  console.log("\nREQ.headers\n", req?.headers);
   if (typeof window === "undefined") {
     // We are on the server
     const instance = axios.create({
       baseURL: SERVER_URI_PRIVATE,
-      headers: req?.headers as any,
       withCredentials: true,
     });
-    // if (req?.headers.cookie !== undefined)
-    //   instance.defaults.headers.common = req?.headers as AxiosRequestHeaders;
+    if (req?.headers.cookie !== undefined)
+      instance.defaults.headers.common.cookie = req?.headers.cookie;
     return instance;
   } else {
     // We are on the client
