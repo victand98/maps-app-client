1371126e33978f9c47d4f89b65f791e80a6a05f5
victand98 <victandres98@gmail.com>
2022-02-27 16:55:55 -0500

    Updating client http config

--- src/lib/helpers/buildClient.ts
+++ src/lib/helpers/buildClient.ts
@@ -3,14 +3,13 @@ import { NextPageContext } from "next";
 import { SERVER_URI, SERVER_URI_PRIVATE } from ".";
 
 const buildClient = ({ req }: NextPageContext) => {
-  console.log("req", req?.headers);
+  console.log("\nreq.headers\n", req?.headers);
   if (typeof window === "undefined") {
     // We are on the server
     return axios.create({
       baseURL: SERVER_URI_PRIVATE,
       headers: {
         cookie: req?.headers.cookie!,
-        "set-cookie": req?.headers["set-cookie"]?.toString()!,
       },
     });
   } else {
