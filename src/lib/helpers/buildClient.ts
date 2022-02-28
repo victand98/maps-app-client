1d67356d3f2ef0293c1b3de8463f3736f23bad28
victand98 <victandres98@gmail.com>
2022-02-27 22:10:20 -0500

    Update request config

--- src/lib/helpers/buildClient.ts
+++ src/lib/helpers/buildClient.ts
@@ -4,7 +4,7 @@ import { SERVER_URI, SERVER_URI_PRIVATE } from ".";
 
 const buildClient = ({ req }: NextPageContext) => {
   if (typeof window === "undefined") {
-    console.log("\nREQ.headers\n", req?.headers);
+    console.log("\nREQ.HEADERS\n", req?.headers);
     // We are on the server
     const instance = axios.create({
       baseURL: SERVER_URI_PRIVATE,
@@ -18,7 +18,7 @@ const buildClient = ({ req }: NextPageContext) => {
     // We are on the client
     return axios.create({
       baseURL: SERVER_URI,
-      // withCredentials: true,
+      withCredentials: true,
     });
   }
 };
