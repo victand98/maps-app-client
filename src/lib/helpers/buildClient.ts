ae3abb858db9cfd2f51883043dca182f6c5b9f8d
victand98 <victandres98@gmail.com>
2022-03-02 00:06:04 -0500

    Fixing Bugs

--- src/lib/helpers/buildClient.ts
+++ src/lib/helpers/buildClient.ts
@@ -5,20 +5,20 @@ import { httpClient, SERVER_URI_PRIVATE } from ".";
 const buildClient = ({ req }: NextPageContext) => {
   if (typeof window === "undefined") {
     // We are on the server
-    console.log("\nREQ.HEADERS\n", req?.headers);
     const instance = axios.create();
     instance.defaults.baseURL = SERVER_URI_PRIVATE;
     instance.defaults.withCredentials = true;
-    instance.defaults.headers.common["Content-Type"] = "application/json";
 
     if (req?.headers.cookie) {
       console.log("\nCOOKIE\n", req?.headers.cookie);
       instance.defaults.headers.common["cookie"] = req?.headers.cookie;
     }
 
+    console.log("instance", instance);
     return instance;
   } else {
     // We are on the client
+    console.log("httpClient", httpClient);
     return httpClient;
   }
 };
