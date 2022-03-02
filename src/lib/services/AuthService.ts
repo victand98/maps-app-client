e05a0738cb1d586a387d89ef062ebb12f8e39c0f
victand98 <victandres98@gmail.com>
2022-03-02 01:50:44 -0500

    Fixing Bugs

--- src/lib/services/AuthService.ts
+++ src/lib/services/AuthService.ts
@@ -1,8 +1,15 @@
-import { httpClient } from "@lib";
+import { httpClient, SERVER_URI } from "@lib";
 import { LoginFormValues } from "@types";
+import axios from "axios";
 
 const AuthService = {
-  login: (data: LoginFormValues) => httpClient.post("/auth/signin", data),
+  login: (data: LoginFormValues) =>
+    axios.post(`${SERVER_URI}/auth/signin`, data, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      withCredentials: false,
+    }),
   logout: () => httpClient.post("/auth/logout"),
 };
 
